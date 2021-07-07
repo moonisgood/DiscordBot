d49987e3fc2eb21ce7c819998baf6d228719ebb1
moonisgood <msy1489@gmail.com>
2021-07-08 02:21:14 +0900

    Update dbManager.js

--- function/dbManager.js
+++ function/dbManager.js
@@ -187,6 +187,8 @@ function UserRankTeamUpdateData(message, players=[], winloseCheck, conn)
                     }
                     let player_name = ` ${displayname} `;
                     let player_points = `[${points}:small_red_triangle_down: -${calPoints}]`;
+                    if((points-calPoints) <= 100)
+                        player_points = `[${points}]`;
                     fields += (player_rank + player_name + player_points + '\n');
                 }
                 else // B팀 승리
@@ -223,8 +225,6 @@ function UserRankTeamUpdateData(message, players=[], winloseCheck, conn)
                     }
                     let player_name = ` ${displayname} `;
                     let player_points = `[${points}:up: +${calPoints}]`;
-                    if((points-calPoints) <= 100)
-                        player_points = `[${points}]`;
                     fields2 += (player_rank + player_name + player_points + '\n');
 
                     if(idx >= 9) // 메세지 출력
